24abb23785a41a7e52e9cca70cb3c7d6f8a02480
Jen Nguyen <jenniferannnguyen@live.com>
2023-05-17 16:20:42 -0600

    Add querySelectors

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 // query selector variables go here 👇
+var poster = document.querySelector('poster');
+var posterImg = document.querySelector('.poster-img');
+var posterTitle = document.querySelector('.poster-quote');
+var posterForm = document.querySelector('.poster-form hidden');
+var posterImgUrl = document.querySelector('#poster-image-url');
+var posterTitle = document.querySelector('#poster-title');
+var posterQuote = document.querySelector('#poster-quote');
+var savePosterButton = document.querySelector('.save-poster');
+var showSavedPostersButton = document.querySelector('.show-saved');
+var showRandomPoster = document.querySelector('.show-random');
+var showForm = document.querySelector('.show-form');
+var makePoster = document.querySelector('.make-poster');
+var showMainButton = document.querySelector('.show-main');
+var backToMainButton = document.querySelector('.back-to-main');
+var showMainButton = document.querySelector('.show-main');
 
-// we've provided you with some data to work with 👇
-//query Selectors and variables here 
+//Global Variables 
 var savedPosters = [];
 var currentPoster;
+// we've provided you with some data to work with 👇
 
 // event listeners go here 👇
 
@@ -12,6 +27,13 @@ var currentPoster;
 // functions here 
 // (we've provided two to get you started)!
 
+// show() {
+
+// }
+
+// hide() {
+
+// }
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
